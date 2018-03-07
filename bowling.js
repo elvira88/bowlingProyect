e8631eabea9a15d92135b4b6bc29675a0df18731
elvira88 <elvira_alvarez1988@hotmail.com>
2018-03-07 23:33:34 +0100

    planteamiento inicial

---
+++ bowling.js
@@ -0,0 +1,16 @@
+//variables globales
+
+var nil = "nil";
+var example = [[1, 2], [10, "nil"], [5, 4], [7, 3], [10, "nil"], [10, "nil"], [1, 4], [6, 2], [7, 3], [10, 3, 7]];
+
+//console.log (example);
+
+if(score<10){
+    //suma del miniarray[a+b]
+}else if(score===10){
+    //semipleno=>10+a
+}else if(score===10nil){
+    //pleno=>10+[a+b]
+}else(score>10){
+    //ultima ronda, 3 tiradas, posibilidad >10, pleno+semi,3 plenos
+}
